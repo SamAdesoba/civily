7cc50f63921eab3e33bec75410e8ddb4d4028add
Adefolarin Samson Adesoba <a.adesoba@native.semicolon.africa>
2023-02-20 22:23:15 +0100

    added atiku api

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ import snscrape.modules.twitter as sntwitter
 import atexit
 import random
 
-model = pickle.load(open('model/model_pickle.pkl', 'rb'))
+model = pickle.load(open('model_training/atiku/atiku_model_pickle.pkl', 'rb'))
 
 vectorizer = CountVectorizer(max_features=1000, ngram_range=(1, 2), max_df=500)
 
-# df = pd.read_csv('util/atiku.csv')
-# df_tweet = ['tweet']
+df = pd.read_csv('util/atiku.csv')
+df_tweet = df['tweet']
 
-last_date = datetime.date.today()
-new_date = datetime.timedelta(hours=24)
+# last_date = datetime.date.today()
+# new_date = datetime.timedelta(hours=24)
 
 
 
@@ -44,27 +44,27 @@ def reformat_json(text):
 app = flask.Flask(__name__)
 
 # @app.route('/home')
-def sensor():
-    result_1 = []
-    search_1 = f'(peterobi OR #peterobi OR #obidatti2023) until:{last_date} since:{last_date - new_date}'
-    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(search_1).get_items()):
-        if i > 10:
-            break
-        else:
-            result_1.append([tweet.date, tweet.user.username, tweet.sourceLabel, tweet.content, tweet.user.location, tweet.likeCount, tweet.retweetCount])
-    df = pd.DataFrame(result_1, columns=['date', 'username', 'sourceLabel', 'tweet', 'location', 'likeCount', 'retweetCount'])
-    return df.to_dict()
+# def sensor():
+#     result_1 = []
+#     search_1 = f'(peterobi OR #peterobi OR #obidatti2023) until:{last_date} since:{last_date - new_date}'
+#     for i, tweet in enumerate(sntwitter.TwitterSearchScraper(search_1).get_items()):
+#         if i > 10:
+#             break
+#         else:
+#             result_1.append([tweet.date, tweet.user.username, tweet.sourceLabel, tweet.content, tweet.user.location, tweet.likeCount, tweet.retweetCount])
+#     df = pd.DataFrame(result_1, columns=['date', 'username', 'sourceLabel', 'tweet', 'location', 'likeCount', 'retweetCount'])
+#     return df.to_dict()
 
 
 
-df_extracted_tweet = sensor().copy()
+# df_extracted_tweet = sensor().copy()
 
-df = pd.DataFrame(df_extracted_tweet)
+# df = pd.DataFrame(df_extracted_tweet)
 
-df_tweet = df['tweet']
+# df_tweet = df['tweet']
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/atiku', methods=['GET', 'POST'])
 def sentiment():
     cleaned_data = df_tweet.apply(cleanText)
 
@@ -78,11 +78,12 @@ def sentiment():
 
     result = model.predict(vectorized_df.values)
 
-    result_df = pd.DataFrame(result, columns=['Analysis'])
+    result_df = pd.DataFrame(result)
 
-    format_result = result_df.value_counts().to_json(orient='columns')
+    # format_result = result_df.value_counts().to_json(orient='index')
 
-    return reformat_json(format_result)
+    # return reformat_json(format_result)
+    return result_df.value_counts().to_json(orient='index')
 
 
 
